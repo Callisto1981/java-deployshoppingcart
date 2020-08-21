676f2f3a775f9c0e127de15fcc176e1a381647e3
Wayne <wabbruscatoorr@gmail.com>
2020-08-20 21:44:13 -0400

    Initial commit

---
+++ deployment/src/test/java/com/lambdaschool/deployment/DeploymentApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
